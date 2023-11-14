b8807c1eef231bd7d508a8b959bce8b87b2ca2a1
Ziping Sun <me@szp.io>
2023-11-15 00:22:30 +0800

    feat(nixos/desktop): use GnuPG to sign commit

--- home.nix
+++ home.nix
@@ -3,10 +3,15 @@
     enable = true;
     userName = "Ziping Sun";
     userEmail = "me@szp.io";
+    extraConfig = {
+      commit.gpgSign = true;
+      init.defaultBranch = "master";
+    };
   };
 
   programs.fish.enable = true;
   programs.gpg.enable = true;
+  services.gpg-agent.enable = true;
   programs.vscode.enable = true;
 
   home.packages = with pkgs; [
