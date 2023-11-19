f9fc4f8c73bd7ed920d259c9eb3bdcecff40b6ee
Ziping Sun <me@szp.io>
2023-11-19 18:05:38 +0800

    feat(nixos/desktop): use GPG for ssh authentication

--- home.nix
+++ home.nix
@@ -11,7 +11,26 @@
 
   programs.fish.enable = true;
   programs.gpg.enable = true;
-  services.gpg-agent.enable = true;
+  # xfce4-session will launch gpg=agent by default.
+  # See <https://docs.xfce.org/xfce/xfce4-session/advanced> for details.
+  # Use the following commands to disable it.
+  # ```shell
+  # xfconf-query -c xfce4-session -p /startup/ssh-agent/enabled -n -t bool -s false
+  # xfconf-query -c xfce4-session -p /startup/gpg-agent/enabled -n -t bool -s false
+  # ```
+  services.gpg-agent = {
+    enable = true;
+    # Yubikey supports multiple ways for ssh authentication and commit signing.
+    # See <https://www.reddit.com/r/yubikey/comments/wzwilj/which_option_to_use_openpgp_piv_fido2sk_keys/>
+    # for a comparison on theses ways.
+    # Keys need to be listed in sshcontrol file, or have `Use-for-ssh: yes` attribute.
+    # ```shell
+    # gpg-connect-agent "KEYATTR 3770D6CF9F129A7A699AECA8248F3FE0BC366A21 Use-for-ssh: yes" /bye
+    # ```
+    # Use `ssh-add -l` to add host keys to sshcontrol file. In this way, when the
+    # card is unplugged, the host key will come before the card key.
+    enableSshSupport = true;
+  };
   programs.vscode.enable = true;
 
   home.packages = with pkgs; [
